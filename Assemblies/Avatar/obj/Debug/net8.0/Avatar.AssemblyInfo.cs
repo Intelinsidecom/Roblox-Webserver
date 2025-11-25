e3af473343fdfd921fe9f0de495b8379764b855c
Intel Inside <www.ponasmakaros@gmail.com>
2025-11-25 16:30:28 +0200

    optimized a bit /develop

--- Assemblies/Avatar/obj/Debug/net8.0/Avatar.AssemblyInfo.cs
+++ Assemblies/Avatar/obj/Debug/net8.0/Avatar.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Avatar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e2cae268b2d8cd42d51b38f8a8d7444c51f8710")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fbb31c0c545bdc2771c21fd69138fa9a15feca2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Avatar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Avatar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
