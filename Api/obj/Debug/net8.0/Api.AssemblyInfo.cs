6b0dadd21e1abfac4d6b5709b4b1fd39866859de
Intel Inside <www.ponasmakaros@gmail.com>
2025-12-05 15:38:02 +0200

    more work

--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9e5656c32ad3010eeb37a3c19142dbda737acdb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e019b82d4657bac07ef79182f3e847f28e3b3435")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
